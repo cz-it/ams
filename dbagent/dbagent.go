951bd4fa7385b75d70ca4625d6d319c353a5af51
C@Z <cz.theng@gmail.com>
2016-04-14 01:09:24 +0800

    add dbagent

---
+++ dbagent/dbagent.go
@@ -0,0 +1,14 @@
+/**
+* Author: CZ cz.theng@gmail.com
+ */
+
+package dbagent
+
+import ()
+
+//DBAgent is a Database proxy
+type DBAgent struct {
+}
+
+//Agent is a siglone DBAgent
+var Agent DBAgent
